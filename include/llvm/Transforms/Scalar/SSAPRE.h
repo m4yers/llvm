740af6328668c4b17a4f3ebe6e2aa32774f34cbf
Artyom Goncharov <m4yers@gmail.com>
2017-03-11 21:06:14 +0200

    [SSAPRE] Add expression and mapping loop

--- include/llvm/Transforms/Scalar/SSAPRE.h
+++ include/llvm/Transforms/Scalar/SSAPRE.h
@@ -15,28 +15,393 @@
 #ifndef LLVM_TRANSFORMS_SCALAR_SSAPRE_H
 #define LLVM_TRANSFORMS_SCALAR_SSAPRE_H
 
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/Analysis/AssumptionCache.h"
+#include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/Dominators.h"
+#include "llvm/Support/ArrayRecycler.h"
 
 namespace llvm {
 
-/// A private "module" namespace for types and utilities used by SSAPRE. These
-/// are implementation details and should not be used by clients.
 namespace ssapre LLVM_LIBRARY_VISIBILITY {
 
 class SSAPRELegacy;
 
+
+//===----------------------------------------------------------------------===//
+// Pass Expressions
+//===----------------------------------------------------------------------===//
+
+enum ExpressionType {
+  ET_Base,
+  ET_Ignored,
+  ET_Unknown,
+  ET_BasicStart,
+  ET_Basic,
+  ET_Phi,
+  ET_Factor, // Phi for expressions, Φ in paper
+  // TODO later
+  // ET_Call,
+  // ET_AggregateValue,
+  // ET_Load,
+  // ET_Store,
+  ET_BasicEnd
+
+};
+
+inline std::string ExpressionTypeToString(ExpressionType ET) {
+  switch (ET) {
+  case ET_Ignored: return "ExpressionTypeIgnored";
+  case ET_Unknown: return "ExpressionTypeUnknown";
+  case ET_Basic:   return "ExpressionTypeBasic";
+  case ET_Phi:     return "ExpressionTypePhi";
+  case ET_Factor:  return "ExpressionTypeFactor";
+  default:         return "ExpressionType???";
+  }
+}
+
+class Expression {
+private:
+  ExpressionType EType;
+  unsigned Opcode;
+
+public:
+  Expression(ExpressionType ET = ET_Base, unsigned O = ~2U)
+      : EType(ET), Opcode(O) {}
+  // Expression(const Expression &) = delete;
+  // Expression &operator=(const Expression &) = delete;
+  virtual ~Expression();
+
+  unsigned getOpcode() const { return Opcode; }
+  void setOpcode(unsigned opcode) { Opcode = opcode; }
+  ExpressionType getExpressionType() const { return EType; }
+
+  // ??? What are those?
+  static unsigned getEmptyKey() { return ~0U; }
+  static unsigned getTombstoneKey() { return ~1U; }
+
+  bool operator==(const Expression &Other) const {
+    if (getOpcode() != Other.getOpcode())
+      return false;
+    if (getOpcode() == getEmptyKey() || getOpcode() == getTombstoneKey())
+      return true;
+    // Compare the expression type for anything but load and store.
+    // For load and store we set the opcode to zero.
+    // This is needed for load coercion.
+    // TODO figure out the reason for this
+    // if (getExpressionType() != ET_Load && getExpressionType() != ET_Store &&
+    //     getExpressionType() != Other.getExpressionType())
+    //   return false;
+
+    return equals(Other);
+  }
+
+  virtual bool equals(const Expression &Other) const { return true; }
+
+  virtual hash_code getHashValue() const {
+    return hash_combine(getExpressionType(), getOpcode());
+  }
+
+  //
+  // Debugging support
+  //
+  virtual void printInternal(raw_ostream &OS) const {
+    OS << ExpressionTypeToString(getExpressionType()) << ", ";
+    OS << "OPC: " << getOpcode() << ", ";
+  }
+
+  void print(raw_ostream &OS) const {
+    OS << "{ ";
+    printInternal(OS);
+    OS << "}";
+  }
+
+  void dump() const { print(dbgs()); }
+}; // class Expression
+
+class IgnoredExpression : public Expression {
+protected:
+  Instruction *Inst;
+
+public:
+  IgnoredExpression(Instruction *I) : IgnoredExpression(I, ET_Ignored) {}
+  IgnoredExpression(Instruction *I, ExpressionType ET) : Expression(ET), Inst(I) {}
+  IgnoredExpression() = delete;
+  IgnoredExpression(const IgnoredExpression &) = delete;
+  IgnoredExpression &operator=(const IgnoredExpression &) = delete;
+  ~IgnoredExpression() override;
+
+  static bool classof(const Expression *EB) {
+    return EB->getExpressionType() == ET_Ignored;
+  }
+
+  Instruction *getInstruction() const { return Inst; }
+  void setInstruction(Instruction *I) { Inst = I; }
+
+  bool equals(const Expression &Other) const override {
+    const auto &OU = cast<IgnoredExpression>(Other);
+    return Expression::equals(Other) && Inst == OU.Inst;
+  }
+
+  hash_code getHashValue() const override {
+    return hash_combine(getExpressionType(), Inst);
+  }
+
+  //
+  // Debugging support
+  //
+  void printInternal(raw_ostream &OS) const override {
+    this->Expression::printInternal(OS);
+    OS << "I = " << *Inst;
+  }
+}; // class IgnoredExpression
+
+class UnknownExpression final : public IgnoredExpression {
+public:
+  UnknownExpression(Instruction *I) : IgnoredExpression(I, ET_Unknown) {}
+  UnknownExpression() = delete;
+  UnknownExpression(const UnknownExpression &) = delete;
+  UnknownExpression &operator=(const UnknownExpression &) = delete;
+  ~UnknownExpression() override;
+
+  static bool classof(const Expression *EB) {
+    return EB->getExpressionType() == ET_Unknown;
+  }
+};
+
+class BasicExpression : public Expression {
+private:
+  typedef ArrayRecycler<Value *> RecyclerType;
+  typedef RecyclerType::Capacity RecyclerCapacity;
+  SmallVector<Value *, 2> Operands;
+  Type *ValueType;
+
+public:
+  BasicExpression()
+      : BasicExpression({}, ET_Basic) {}
+  BasicExpression(SmallVector<Value *, 2> O)
+      : BasicExpression(O, ET_Basic) {}
+  BasicExpression(SmallVector<Value *, 2> O, ExpressionType ET)
+      : Expression(ET), Operands(O) {}
+  // BasicExpression(const BasicExpression &) = delete;
+  // BasicExpression &operator=(const BasicExpression &) = delete;
+  ~BasicExpression() override;
+
+  static bool classof(const Expression *EB) {
+    ExpressionType ET = EB->getExpressionType();
+    return ET > ET_BasicStart && ET < ET_BasicEnd;
+  }
+
+  void swapOperands(unsigned First, unsigned Second) {
+    std::swap(Operands[First], Operands[Second]);
+  }
+
+  Value *getOperand(unsigned N) const {
+    return Operands[N];
+  }
+
+  SmallVector<Value *, 2>& getOperands() {
+    return Operands;
+  }
+
+  void addOperand(Value *V) {
+    Operands.push_back(V);
+  }
+
+  void setOperand(unsigned N, Value *V) {
+    assert(N < Operands.size() && "Operand out of range");
+    Operands[N] = V;
+  }
+
+  unsigned getNumOperands() const { return Operands.size(); }
+
+  void setType(Type *T) { ValueType = T; }
+  Type *getType() const { return ValueType; }
+
+  bool equals(const Expression &Other) const override {
+    if (getOpcode() != Other.getOpcode())
+      return false;
+
+    const auto &OE = cast<BasicExpression>(Other);
+    return getType() == OE.getType() && Operands == OE.Operands;
+  }
+
+  hash_code getHashValue() const override {
+    return hash_combine(getExpressionType(), getOpcode(), ValueType,
+        hash_combine_range(Operands.begin(), Operands.end()));
+  }
+
+  //
+  // Debugging support
+  //
+  void printInternal(raw_ostream &OS) const override {
+    this->Expression::printInternal(OS);
+    OS << "OPS: { ";
+    for (unsigned i = 0, e = getNumOperands(); i != e; ++i) {
+      OS << "[" << i << "] = ";
+      Operands[i]->printAsOperand(OS);
+      if (i + 1 != e)
+        OS << ", ";
+    }
+    OS << " }";
+  }
+}; // class BasicExpression
+
+class PHIExpression final : public BasicExpression {
+private:
+  BasicBlock *BB;
+
+public:
+  PHIExpression(SmallVector<Value *, 2> O, BasicBlock *BB)
+      : BasicExpression(O, ET_Phi), BB(BB) {}
+  PHIExpression() = default;
+  // PHIExpression(const PHIExpression &) = delete;
+  // PHIExpression &operator=(const PHIExpression &) = delete;
+  ~PHIExpression() override;
+
+  static bool classof(const Expression *EB) {
+    return EB->getExpressionType() == ET_Phi;
+  }
+
+  bool equals(const Expression &Other) const override {
+    if (!this->BasicExpression::equals(Other))
+      return false;
+    const PHIExpression &OE = cast<PHIExpression>(Other);
+    return BB == OE.BB;
+  }
+
+  hash_code getHashValue() const override {
+    return hash_combine(this->BasicExpression::getHashValue(), BB);
+  }
+
+  //
+  // Debugging support
+  //
+  void printInternal(raw_ostream &OS) const override {
+    this->BasicExpression::printInternal(OS);
+    OS << "BB = " << BB->getName();
+  }
+}; // class PHIExpression
+
+class FactorExpression final : public Expression {
+private:
+  BasicBlock *BB;
+  std::vector<Expression *> Expressions;
+
+public:
+  FactorExpression(BasicBlock *BB, std::vector<Expression *> E)
+      : Expression(ET_Factor), BB(BB), Expressions(E) {}
+  FactorExpression() = delete;
+  FactorExpression(const FactorExpression &) = delete;
+  FactorExpression &operator=(const FactorExpression &) = delete;
+  ~FactorExpression() override;
+
+  static bool classof(const Expression *EB) {
+    return EB->getExpressionType() == ET_Factor;
+  }
+
+  bool equals(const Expression &Other) const override {
+    if (!this->Expression::equals(Other))
+      return false;
+    const FactorExpression &OE = cast<FactorExpression>(Other);
+    return BB == OE.BB;
+  }
+
+  hash_code getHashValue() const override {
+    return hash_combine(this->Expression::getHashValue(), BB);
+  }
+
+  //
+  // Debugging support
+  //
+  void printInternal(raw_ostream &OS) const override {
+    this->Expression::printInternal(OS);
+    OS << "BB = " << BB->getName();
+  }
+}; // class FactorExpression
 } // end namespace ssapre
+
+using namespace ssapre;
+
+template <> struct DenseMapInfo<const Expression *> {
+  static const Expression *getEmptyKey() {
+    auto Val = static_cast<uintptr_t>(-1);
+    Val <<= PointerLikeTypeTraits<const Expression *>::NumLowBitsAvailable;
+    return reinterpret_cast<const Expression *>(Val);
+  }
+  static const Expression *getTombstoneKey() {
+    auto Val = static_cast<uintptr_t>(~1U);
+    Val <<= PointerLikeTypeTraits<const Expression *>::NumLowBitsAvailable;
+    return reinterpret_cast<const Expression *>(Val);
+  }
+  static unsigned getHashValue(const Expression *V) {
+    return static_cast<unsigned>(V->getHashValue());
+  }
+  static bool isEqual(const Expression *LHS, const Expression *RHS) {
+    if (LHS == RHS)
+      return true;
+    if (LHS == getTombstoneKey() || RHS == getTombstoneKey() ||
+        LHS == getEmptyKey() || RHS == getEmptyKey())
+      return false;
+    return LHS->equals(*RHS);
+  }
+};
 
 /// Performs SSA PRE pass.
 class SSAPRE : public PassInfoMixin<SSAPRE> {
+  DominatorTree *DT;
+  const DataLayout *DL;
+  const TargetLibraryInfo *TLI;
+  AssumptionCache *AC;
+
+  // Number of function arguments, used by ranking
+  unsigned int NumFuncArgs;
+
+  // DFS info.
+  // This contains a mapping from Instructions to DFS numbers.
+  // The numbering starts at 1. An instruction with DFS number zero
+  // means that the instruction is dead.
+  DenseMap<const Value *, unsigned> InstrDFS;
+
+  // Expression-to-Definitions map
+  DenseMap<const Expression *, SmallPtrSet<const Value *, 5>> ExpressionToInsts;
+
 public:
   PreservedAnalyses run(Function &F, AnalysisManager<Function> &AM);
 
 private:
   friend ssapre::SSAPRELegacy;
 
-  PreservedAnalyses runImpl(Function &F, DominatorTree &DT);
+  std::pair<unsigned, unsigned> AssignDFSNumbers(BasicBlock *B, unsigned Start);
+
+  // This function provides global ranking of operations so that we can place them
+  // in a canonical order.  Note that rank alone is not necessarily enough for a
+  // complete ordering, as constants all have the same rank.  However, generally,
+  // we will simplify an operation with all constants so that it doesn't matter
+  // what order they appear in.
+  unsigned int GetRank(const Value *V) const;
+
+  // This is a function that says whether two commutative operations should
+  // have their order swapped when canonicalizing.
+  bool ShouldSwapOperands(const Value *A, const Value *B) const;
+
+  bool FillInBasicExpressionInfo(Instruction &I, BasicExpression *E);
+
+  // Take a Value returned by simplification of Expression E/Instruction
+  // I, and see if it resulted in a simpler expression. If so, return
+  // that expression.
+  // TODO: Once finished, this should not take an Instruction, we only
+  // use it for printing.
+  Expression * CheckSimplificationResults(Expression *E,
+                                          Instruction &I, Value *V);
+  Expression * CreateIgnoredExpression(Instruction &I);
+  Expression * CreateUnknownExpression(Instruction &I);
+  Expression * CreateBasicExpression(Instruction &I);
+  Expression * CreatePHIExpression(Instruction &I);
+  Expression * CreateExpression(Instruction &I);
+  PreservedAnalyses runImpl(Function &F, AssumptionCache &_AC,
+                            TargetLibraryInfo &_TLI, DominatorTree &_DT);
 };
 } // end namespace llvm
 
--- lib/Transforms/Scalar/SSAPRE.cpp
+++ lib/Transforms/Scalar/SSAPRE.cpp
@@ -11,6 +11,9 @@
 #include "llvm/Transforms/Scalar/SSAPRE.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils/BreakCriticalEdges.h"
+#include "llvm/Analysis/IteratedDominanceFrontier.h"
+#include "llvm/Analysis/InstructionSimplify.h"
+#include "llvm/Analysis/ConstantFolding.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/IRBuilder.h"
@@ -32,6 +35,10 @@
 #include "llvm/ADT/SparseBitVector.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/ADT/TinyPtrVector.h"
+#include "llvm/Support/Allocator.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 using namespace llvm::ssapre;
@@ -44,16 +51,349 @@ STATISTIC(SSAPREInstrInserted, "Number of instructions inserted");
 STATISTIC(SSAPREInstrDeleted,  "Number of instructions deleted");
 STATISTIC(SSAPREBlocksAdded,   "Number of blocks deleted");
 
+// Anchor methods.
+namespace llvm {
+namespace ssapre {
+Expression::~Expression() = default;
+IgnoredExpression::~IgnoredExpression() = default;
+UnknownExpression::~UnknownExpression() = default;
+BasicExpression::~BasicExpression() = default;
+PHIExpression::~PHIExpression() = default;
+FactorExpression::~FactorExpression() = default;
+}
+}
 
 //===----------------------------------------------------------------------===//
 // Pass Implementation
 //===----------------------------------------------------------------------===//
 
-PreservedAnalyses SSAPRE::runImpl(Function &F, DominatorTree &DT) {
+std::pair<unsigned, unsigned> SSAPRE::
+AssignDFSNumbers(BasicBlock *B, unsigned Start) {
+  unsigned End = Start;
+  // if (MemoryAccess *MemPhi = MSSA->getMemoryAccess(B)) {
+  //   InstrDFS[MemPhi] = End++;
+  //   DFSToInstr.emplace_back(MemPhi);
+  // }
+
+  for (auto &I : *B) {
+    InstrDFS[&I] = End++;
+    // DFSToInstr.emplace_back(&I);
+  }
+
+  // All of the range functions taken half-open ranges (open on the end side).
+  // So we do not subtract one from count, because at this point it is one
+  // greater than the last instruction.
+  return std::make_pair(Start, End);
+}
+
+unsigned int SSAPRE::
+GetRank(const Value *V) const {
+  // Prefer undef to anything else
+  if (isa<UndefValue>(V))
+    return 0;
+  if (isa<Constant>(V))
+    return 1;
+  else if (auto *A = dyn_cast<Argument>(V))
+    return 2 + A->getArgNo();
+
+  // Need to shift the instruction DFS by number of arguments + 3 to account for
+  // the constant and argument ranking above.
+  unsigned Result = InstrDFS.lookup(V);
+  if (Result > 0)
+    return 3 + NumFuncArgs + Result;
+  // Unreachable or something else, just return a really large number.
+  return ~0;
+}
+
+bool SSAPRE::
+ShouldSwapOperands(const Value *A, const Value *B) const {
+  // Because we only care about a total ordering, and don't rewrite expressions
+  // in this order, we order by rank, which will give a strict weak ordering to
+  // everything but constants, and then we order by pointer address.
+  return std::make_pair(GetRank(A), A) > std::make_pair(GetRank(B), B);
+}
+
+bool SSAPRE::
+FillInBasicExpressionInfo(Instruction &I, BasicExpression *E) {
+  bool AllConstant = true;
+  if (auto *GEP = dyn_cast<GetElementPtrInst>(&I)) {
+    E->setType(GEP->getSourceElementType());
+  } else {
+    E->setType(I.getType());
+  }
+
+  E->setOpcode(I.getOpcode());
+
+  for (auto &O : I.operands()) {
+    AllConstant &= isa<Constant>(O);
+    E->addOperand(O);
+  }
+
+  return AllConstant;
+}
+
+Expression *SSAPRE::
+CheckSimplificationResults(Expression *E, Instruction &I, Value *V) {
+  if (!V)
+    return nullptr;
+
+  if (auto *C = dyn_cast<Constant>(V)) {
+    DEBUG(dbgs() << "Simplified " << I << " to "
+                 << " constant " << *C << "\n");
+    assert(isa<BasicExpression>(E) &&
+           "We should always have had a basic expression here");
+
+    // cast<BasicExpression>(E)->deallocateOperands(ArgRecycler);
+    // ExpressionAllocator.Deallocate(E);
+    return CreateIgnoredExpression(I);
+  } else if (isa<Argument>(V) || isa<GlobalVariable>(V)) {
+    DEBUG(dbgs() << "Simplified " << I << " to "
+                 << " variable " << *V << "\n");
+    // cast<BasicExpression>(E)->deallocateOperands(ArgRecycler);
+    // ExpressionAllocator.Deallocate(E);
+    return CreateIgnoredExpression(I);
+  }
+
+  return nullptr;
+}
+
+Expression * SSAPRE::
+CreateIgnoredExpression(Instruction &I) {
+  // auto *E = new (ExpressionAllocator) UnknownExpression(I);
+  auto *E = new IgnoredExpression(&I);
+  E->setOpcode(I.getOpcode());
+  return E;
+}
+
+Expression * SSAPRE::
+CreateUnknownExpression(Instruction &I) {
+  // auto *E = new (ExpressionAllocator) UnknownExpression(I);
+  auto *E = new UnknownExpression(&I);
+  E->setOpcode(I.getOpcode());
+  return E;
+}
+
+Expression * SSAPRE::
+CreateBasicExpression(Instruction &I) {
+  // auto *E = new (ExpressionAllocator) BasicExpression(I->getNumOperands());
+  auto *E = new BasicExpression();
+
+  bool AllConstant = FillInBasicExpressionInfo(I, E);
+
+  if (I.isCommutative()) {
+    // Ensure that commutative instructions that only differ by a permutation
+    // of their operands get the same expression map by sorting the operand value
+    // numbers.  Since all commutative instructions have two operands it is more
+    // efficient to sort by hand rather than using, say, std::sort.
+    assert(I.getNumOperands() == 2 && "Unsupported commutative instruction!");
+    if (ShouldSwapOperands(E->getOperand(0), E->getOperand(1)))
+      E->swapOperands(0, 1);
+  }
+
+  // Perform simplificaiton
+  // We do not actually require simpler instructions but rather require them be
+  // in a canonical form. Mainly we are interested in instructions that we
+  // ignore, such as constants and variables.
+  // TODO: Right now we only check to see if we get a constant result.
+  // We may get a less than constant, but still better, result for
+  // some operations.
+  // IE
+  //  add 0, x -> x
+  //  and x, x -> x
+  // We should handle this by simply rewriting the expression.
+  if (auto *CI = dyn_cast<CmpInst>(&I)) {
+    // Sort the operand value numbers so x<y and y>x get the same value
+    // number.
+    CmpInst::Predicate Predicate = CI->getPredicate();
+    if (ShouldSwapOperands(E->getOperand(0), E->getOperand(1))) {
+      E->swapOperands(0, 1);
+      Predicate = CmpInst::getSwappedPredicate(Predicate);
+    }
+    E->setOpcode((CI->getOpcode() << 8) | Predicate);
+    // TODO: 25% of our time is spent in SimplifyCmpInst with pointer operands
+    assert(I.getOperand(0)->getType() == I.getOperand(1)->getType() &&
+           "Wrong types on cmp instruction");
+    assert((E->getOperand(0)->getType() == I.getOperand(0)->getType() &&
+            E->getOperand(1)->getType() == I.getOperand(1)->getType()));
+    Value *V = SimplifyCmpInst(Predicate, E->getOperand(0), E->getOperand(1),
+                               *DL, TLI, DT, AC);
+    if (auto *SE = CheckSimplificationResults(E, I, V))
+      return SE;
+  } else if (isa<SelectInst>(I)) {
+    if (isa<Constant>(E->getOperand(0)) ||
+        E->getOperand(0) == E->getOperand(1)) {
+      assert(E->getOperand(1)->getType() == I.getOperand(1)->getType() &&
+             E->getOperand(2)->getType() == I.getOperand(2)->getType());
+      Value *V = SimplifySelectInst(E->getOperand(0), E->getOperand(1),
+                                    E->getOperand(2), *DL, TLI, DT, AC);
+      if (auto *SE = CheckSimplificationResults(E, I, V))
+        return SE;
+    }
+  } else if (I.isBinaryOp()) {
+    Value *V = SimplifyBinOp(E->getOpcode(), E->getOperand(0), E->getOperand(1),
+                             *DL, TLI, DT, AC);
+    if (auto *SE = CheckSimplificationResults(E, I, V))
+      return SE;
+  } else if (auto *BI = dyn_cast<BitCastInst>(&I)) {
+    Value *V = SimplifyInstruction(BI, *DL, TLI, DT, AC);
+    if (auto *SE = CheckSimplificationResults(E, I, V))
+      return SE;
+  } else if (isa<GetElementPtrInst>(I)) {
+    Value *V = SimplifyGEPInst(E->getType(), E->getOperands(), *DL, TLI, DT, AC);
+    if (auto *SE = CheckSimplificationResults(E, I, V))
+      return SE;
+  } else if (AllConstant) {
+    // We don't bother trying to simplify unless all of the operands
+    // were constant.
+    // TODO: There are a lot of Simplify*'s we could call here, if we
+    // wanted to.  The original motivating case for this code was a
+    // zext i1 false to i8, which we don't have an interface to
+    // simplify (IE there is no SimplifyZExt).
+
+    SmallVector<Constant *, 8> C;
+    for (Value *Arg : E->getOperands())
+      C.emplace_back(cast<Constant>(Arg));
+
+    if (Value *V = ConstantFoldInstOperands(&I, C, *DL, TLI))
+      if (auto *SE = CheckSimplificationResults(E, I, V))
+        return SE;
+  }
+
+  return E;
+}
+
+Expression *SSAPRE::
+CreatePHIExpression(Instruction &I) {
+  auto *E = new PHIExpression();
+  FillInBasicExpressionInfo(I, E);
+  // Very simple method, we do not try check for undef etc
+  return E;
+}
+
+Expression *
+SSAPRE::CreateExpression(Instruction &I) {
+  Expression * E = nullptr;
+  switch (I.getOpcode()) {
+  case Instruction::ExtractValue:
+  case Instruction::InsertValue:
+    // E = performSymbolicAggrValueEvaluation(I);
+    break;
+  case Instruction::PHI:
+    E = CreatePHIExpression(I);
+    break;
+  case Instruction::Call:
+    // E = performSymbolicCallEvaluation(I);
+    break;
+  case Instruction::Store:
+    // E = performSymbolicStoreEvaluation(I);
+    break;
+  case Instruction::Load:
+    // E = performSymbolicLoadEvaluation(I);
+    break;
+  case Instruction::BitCast: {
+    E = CreateBasicExpression(I);
+  } break;
+  case Instruction::ICmp:
+  case Instruction::FCmp: {
+    // E = performSymbolicCmpEvaluation(I);
+  } break;
+  case Instruction::Add:
+  case Instruction::FAdd:
+  case Instruction::Sub:
+  case Instruction::FSub:
+  case Instruction::Mul:
+  case Instruction::FMul:
+  case Instruction::UDiv:
+  case Instruction::SDiv:
+  case Instruction::FDiv:
+  case Instruction::URem:
+  case Instruction::SRem:
+  case Instruction::FRem:
+  case Instruction::Shl:
+  case Instruction::LShr:
+  case Instruction::AShr:
+  case Instruction::And:
+  case Instruction::Or:
+  case Instruction::Xor:
+  case Instruction::Trunc:
+  case Instruction::ZExt:
+  case Instruction::SExt:
+  case Instruction::FPToUI:
+  case Instruction::FPToSI:
+  case Instruction::UIToFP:
+  case Instruction::SIToFP:
+  case Instruction::FPTrunc:
+  case Instruction::FPExt:
+  case Instruction::PtrToInt:
+  case Instruction::IntToPtr:
+  case Instruction::Select:
+  case Instruction::ExtractElement:
+  case Instruction::InsertElement:
+  case Instruction::ShuffleVector:
+  case Instruction::GetElementPtr:
+    E = CreateBasicExpression(I);
+    break;
+  default:
+    E = CreateUnknownExpression(I);
+  }
+
+  if (!E)
+    E = CreateUnknownExpression(I);
+
+  return E;
+}
+
+PreservedAnalyses
+SSAPRE::runImpl(Function &F, AssumptionCache &_AC,
+                TargetLibraryInfo &_TLI, DominatorTree &_DT) {
   bool Changed = false;
 
-  errs() << "Hello: ";
-  errs().write_escaped(F.getName()) << '\n';
+  DT = &_DT;
+  TLI = &_TLI;
+  DL = &F.getParent()->getDataLayout();
+  AC = &_AC;
+
+  NumFuncArgs = F.arg_size();
+
+  // Assign each instruction a DFS order number
+  unsigned ICount = 0;
+  auto DFI = df_begin(DT->getRootNode());
+  for (auto DFE = df_end(DT->getRootNode()); DFI != DFE; ++DFI) {
+    BasicBlock *B = DFI->getBlock();
+    const auto &BlockRange = AssignDFSNumbers(B, ICount);
+    // BlockInstRange.insert({B, BlockRange});
+    ICount += BlockRange.second - BlockRange.first;
+  }
+
+  // Collect all expressions
+  ReversePostOrderTraversal<Function *> RPOT(&F);
+  for (auto &BB : RPOT) {
+    for (auto &I : *BB) {
+      // We map every instruction except terminators
+      if (!I.isTerminator()) {
+        auto E = CreateExpression(I);
+        assert(E && "Oh No!");
+        if (!ExpressionToInsts.count(E)) {
+          ExpressionToInsts.insert({E, {&I}});
+        } else {
+          ExpressionToInsts[E].insert(&I);
+        }
+      }
+    }
+  }
+
+  DEBUG(
+      dbgs() << "ExpressionsToInts\n";
+      for (auto &P : ExpressionToInsts) {
+      dbgs() << "(" << P.getSecond().size() << ") ";
+        P.getFirst()->printInternal(dbgs());
+        dbgs() << ":";
+        for (const auto &E : P.getSecond()) {
+          dbgs() << "\n" << *E;
+        }
+        dbgs() << "\n";
+      }
+  );
 
   if (!Changed)
     return PreservedAnalyses::all();
@@ -62,7 +402,10 @@ PreservedAnalyses SSAPRE::runImpl(Function &F, DominatorTree &DT) {
 }
 
 PreservedAnalyses SSAPRE::run(Function &F, AnalysisManager<Function> &AM) {
-  return runImpl(F, AM.getResult<DominatorTreeAnalysis>(F));
+  return runImpl(F,
+      AM.getResult<AssumptionAnalysis>(F),
+      AM.getResult<TargetLibraryAnalysis>(F),
+      AM.getResult<DominatorTreeAnalysis>(F));
 }
 
 
@@ -88,14 +431,18 @@ public:
     if (skipFunction(F))
       return false;
 
+    auto &AC = getAnalysis<AssumptionCacheTracker>().getAssumptionCache(F);
     auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
-    auto PA = Impl.runImpl(F, DT);
+    auto &TLI = getAnalysis<TargetLibraryInfoWrapperPass>().getTLI();
+    auto PA = Impl.runImpl(F, AC, TLI, DT);
     return !PA.areAllPreserved();
   }
 
 private:
   void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<AssumptionCacheTracker>();
     AU.addRequired<DominatorTreeWrapperPass>();
+    AU.addRequired<TargetLibraryInfoWrapperPass>();
   }
 };
 
@@ -109,7 +456,9 @@ INITIALIZE_PASS_BEGIN(SSAPRELegacy,
                       "SSA Partial Redundancy Elimination",
                       false, false)
 INITIALIZE_PASS_DEPENDENCY(BreakCriticalEdges)
+INITIALIZE_PASS_DEPENDENCY(AssumptionCacheTracker)
 INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
+INITIALIZE_PASS_DEPENDENCY(TargetLibraryInfoWrapperPass)
 INITIALIZE_PASS_END(SSAPRELegacy,
                     "ssapre",
                     "SSA Partial Redundancy Elimination",
